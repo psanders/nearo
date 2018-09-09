26b6721b445f1b4d44ddcbf640e7b9a0e6c2f7d2
Fonoster Team <fonosterteam@fonoster.com>
2018-09-09 18:35:19 -0400

    Save user the first time it login

--- src/components/commons/firebase/auth.js
+++ src/components/commons/firebase/auth.js
@@ -1,15 +1,37 @@
-import { auth, googleProvider, fbProvider } from './firebase';
+import {
+  auth,
+  googleProvider,
+  fbProvider,
+  db
+} from './firebase';
 
 // Sign In
-export const doRedirectSignIn = () =>
-  auth.signInWithRedirect(googleProvider);
+export const doRedirectSignIn = () => auth.signInWithRedirect(googleProvider);
 
-export const doSignInWithGoogle = () =>
-  auth.signInWithRedirect(googleProvider);
+export const doSignInWithGoogle = () => auth.signInWithRedirect(googleProvider);
 
-export const doSignInWithFacebook = () =>
-  auth.signInWithRedirect(fbProvider);
+export const doSignInWithFacebook = () => auth.signInWithRedirect(fbProvider);
 
-// Sign out
-export const doSignOut = () =>
-  auth.signOut();
+export const doSignOut = () => auth.signOut();
+
+auth.getRedirectResult().then(function(result) {
+  if (result.user && result.additionalUserInfo.isNewUser) {
+      const profile = result.additionalUserInfo.profile;
+      const user = {
+        email: profile.email,
+        name: profile.name,
+        gender: profile.gender,
+        picture: profile.picture
+      }
+
+      db.collection('users')
+      .add(user)
+      .then(function(docRef) {
+      })
+      .catch(function(error) {
+        console.error("Error adding document: ", error);
+      });
+  }
+}).catch(function(error) {
+  console.error(error);
+});
