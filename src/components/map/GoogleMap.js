2ca4c237c5d7c96d7fb2b7830d02811600d9a9c7
Fonoster Team <fonosterteam@fonoster.com>
2018-09-14 14:09:49 -0400

    Center map

--- src/components/map/GoogleMap.js
+++ src/components/map/GoogleMap.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import { fetchUserInfo } from '../commons/dbfunctions';
 
 class SimpleMap extends Component {
+  state = {
+    center: {}
+  }
+
   static defaultProps = {
     center: {
       lat: 59.95,
@@ -9,6 +14,15 @@ class SimpleMap extends Component {
     },
     zoom: 11
   };
+
+  componentDidMount = () => {
+    fetchUserInfo('topnav-locator')
+    .then(locInfo => {
+      if (locInfo) {
+        this.setState({center: locInfo.latLng})
+      }
+    })
+  }
 
   render() {
     return (
@@ -18,6 +32,7 @@ class SimpleMap extends Component {
           bootstrapURLKeys={{ key: "AIzaSyBJWWg7cJV5835KCpmNsG2D2UwBbs0EY9Y" }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
+          center={this.state.center}
         />
       </div>
     );
