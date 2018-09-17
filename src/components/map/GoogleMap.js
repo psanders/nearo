83252cf8c9d5b9ae4e4eb4b92a2621b0b05c03e6
Fonoster Team <fonosterteam@fonoster.com>
2018-09-17 08:01:42 -0400

    Improves geolocation

--- src/components/map/GoogleMap.js
+++ src/components/map/GoogleMap.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react'
 import GoogleMapReact from 'google-map-react'
 import Marker from './Marker'
 import { observer } from 'mobx-react'
+import { when } from "mobx"
 
 @observer
 class GMap extends Component {
+  state = {
+    map: null,
+    maps: null
+  }
 
   static defaultProps = {
     center: {
@@ -16,16 +21,46 @@ class GMap extends Component {
 
   render() {
 
+    const posts = this.props.postsStore.posts
+
+    const getMapBounds = (map, maps, posts) => {
+      const bounds = new maps.LatLngBounds();
+
+      posts.forEach((post) => {
+        bounds.extend(new maps.LatLng(
+          post._geoloc.lat,
+          post._geoloc.lng,
+        ));
+      });
+      return bounds;
+    }
+
+    const centerMap = (map, maps, posts) => {
+      const bounds = getMapBounds(map, maps, posts);
+      map.fitBounds(bounds);
+    }
+
+    when(
+      () => this.state.map
+            && this.state.maps
+            && this.props.postsStore.posts,
+      () => {
+        centerMap(this.state.map, this.state.maps, posts.slice(0, 10))
+    })
+
     return (
       <div style={{ height: 'calc(100vh - 65px)', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: "AIzaSyBJWWg7cJV5835KCpmNsG2D2UwBbs0EY9Y" }}
-          defaultCenter= { this.props.center }
+          defaultCenter= { this.props.navStore.navInfo.locInfo.latLng }
           defaultZoom={ this.props.zoom }
-          center={ this.props.navStore.navInfo.locInfo.latLng }
+          onGoogleApiLoaded={({ map, maps }) => {
+            this.state.map = map
+            this.state.maps = maps
+          }}
         >
           {
-            this.props.postsStore.posts.map(post =>
+            posts.map(post =>
             <Marker post={post} key={ post.id } latLng={ post._geoloc } />)
           }
         </GoogleMapReact>
