d2edfac211f33dad73768d6b236a88d38306d4e7
Fonoster Team <fonosterteam@fonoster.com>
2018-09-30 08:00:18 -0400

    Cleanup

--- src/components/404.js
+++ src/components/404.js
@@ -6,7 +6,7 @@ import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
-import { Route, Switch, withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 
 @withRouter
 class NoMatch extends Component {
