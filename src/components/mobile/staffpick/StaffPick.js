970030954f5b03f723e82f884a810222dbe4b77f
Fonoster Team <fonosterteam@fonoster.com>
2018-11-17 21:42:35 -0500

    Improved the SP component

--- src/components/mobile/staffpick/StaffPick.js
+++ src/components/mobile/staffpick/StaffPick.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
+import Avatar from '@material-ui/core/Avatar'
 import GridList from '@material-ui/core/GridList'
 import GridListTile from '@material-ui/core/GridListTile'
 import GridListTileBar from '@material-ui/core/GridListTileBar'
+import blue from '@material-ui/core/colors/blue'
 import { observer, inject } from 'mobx-react'
 import { computed } from 'mobx'
 
@@ -24,11 +26,25 @@ const styles = theme => ({
   },
   title: {
     color: '#fff',
+    fontSize: 12
   },
   titleBar: {
     background:
       'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
   },
+  listTile: {
+    borderRadius: 4,
+    height: 180
+  },
+  avatar: {
+    zIndex: 1000,
+    width: 35,
+    height: 35,
+    position: 'absolute',
+    top: 5,
+    left: 5,
+    border: '3px solid ' + theme.palette.primary.main
+  }
 })
 
 @inject('postsStore')
@@ -48,7 +64,8 @@ class StaffPick extends Component {
         <GridList className={classes.gridList} cols={2.2}>
           {this.posts.map(post => (
             post.media.length > 0 &&
-            <GridListTile key={post.id}>
+            <GridListTile key={post.id} classes={{tile: classes.listTile}}>
+              <Avatar classes={{root:classes.avatar}} src={post.avatar} />
               <img onClick={() => routing.push('/posts/' + post.id)} src={imageURL(post, 'md')} alt={post.title} />
               <GridListTileBar
                 title={post.title}
